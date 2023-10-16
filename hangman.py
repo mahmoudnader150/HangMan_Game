47c271f3b1baeea8bede38facf6141e268c96be1
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-10-16 16:10:35 +0300

    create guess entry and button

--- hangman.py
+++ hangman.py
@@ -34,6 +34,12 @@ word_with_blanks = '_' * len(word)
 word_label = tk.Label(root,text=word_with_blanks,font=("Arial",24))
 word_label.grid(row=1,column=0)
 
+#create guess entry and button
+guess_entry = tk.Entry(root,width=3,font=("Arial",24))
+guess_entry.grid(row=2,column=0)
+guess_button=tk.Button(root,text="Guess",font=("Arial",12))
+guess_button.grid(row=2,column=1)
+
 
 
 #start event loop
