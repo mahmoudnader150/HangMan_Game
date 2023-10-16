3a0cd6bbcdb90d785aa2ba3f8c04c624c6bff0a7
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-10-16 15:44:46 +0300

    define hangman art

--- HangMan_Game.py
+++ HangMan_Game.py
@@ -4,3 +4,13 @@ import random
 #define list of random words
 words = ['python','programming','computer','algorithm','function','variable','syntax']
 
+#define the ASCII aret for hangman
+hangman_art = [
+    "   +---+\n   |   |\n       |\n       |\n       |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n       |\n       |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n   |   |\n       |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n  /|   |\n       |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n  /|\\  |\n       |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n  /|\\  |\n  /    |\n       |\n=========",
+    "   +---+\n   |   |\n   O   |\n  /|\\  |\n  / \\  |\n       |\n========="
+]
